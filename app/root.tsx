1cfb21fdad7f05a6f09be10d9fd999f08a4fd148
Vaibhav <sharmavaibhav227@gmail.com>
2025-05-01 12:55:46 +0530

    auth

--- app/root.tsx
+++ app/root.tsx
@@ -67,7 +67,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
           <QueryClientProvider client={queryClient}>
-            {false ? <Authentication /> : <Page children={children} />}
+            {!user.email ? <Authentication /> : <Page children={children} />}
           </QueryClientProvider>
           <Analytics />
         </ThemeProvider>
@@ -84,7 +84,7 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {false ? <Authentication /> : <Outlet />}
+      {!user.email ? <Authentication /> : <Outlet />}
     </QueryClientProvider>
   )
 }
